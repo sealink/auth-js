90bb39164465acb2dd3ae5e53e83fc380afdddd6
John Mortlock <john.mortlock@gmail.com>
2019-12-23 12:25:13 +1030

    qq

--- src/authorizer.js
+++ src/authorizer.js
@@ -8,21 +8,21 @@ const fetch = require("node-fetch");
 const generatePolicy = (principalId, effect, resource) => {
   const authResponse = {};
   authResponse.principalId = principalId;
-  if (effect && resource) {
-    const policyDocument = {};
-    policyDocument.Version = "2012-10-17";
-    policyDocument.Statement = [];
-    const statementOne = {};
-    statementOne.Action = "execute-api:Invoke";
-    statementOne.Effect = effect;
-    statementOne.Resource = resource;
-    policyDocument.Statement[0] = statementOne;
-    authResponse.policyDocument = policyDocument;
-  }
+
+  const policyDocument = {};
+  policyDocument.Version = "2012-10-17";
+  policyDocument.Statement = [];
+  const statementOne = {};
+  statementOne.Action = "execute-api:Invoke";
+  statementOne.Effect = effect;
+  statementOne.Resource = resource;
+  policyDocument.Statement[0] = statementOne;
+  authResponse.policyDocument = policyDocument;
+
   return authResponse;
 };
 
-const authorize = async (event, issuer, options) => {
+const authorize = async (event, issuer) => {
   if (event.authorizationToken) {
     const token = event.authorizationToken.substring(7);
 
